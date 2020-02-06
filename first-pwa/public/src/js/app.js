01201db9e21196502c786afa296f3492e7773b4d
Mainul Hasan <mainul@mvisolutions.com>
2020-02-06 17:45:09 +0600

    Include third party library for promise and fetch if older browser doesn't support

--- first-pwa/public/src/js/app.js
+++ first-pwa/public/src/js/app.js
@@ -1,10 +1,17 @@
 var deferredPrompt;
 
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
 		.register('/sw.js')
 		.then(function() {
 			console.log('Service worker registered!');
+		})
+		.catch(function(err) {
+			console.log(err);
 		});
 }
 
@@ -29,6 +36,57 @@ var promise = new Promise(function(resolve, reject) {
   	// console.log('This is executed once the timer is done!');
   }, 3000);	
 });
+
+
+// Traditional Ajax request
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://httpbin.org/ip');
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+	console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+	console.log('Error!');
+};
+
+xhr.send();
+
+
+
+fetch('http://httpbin.org/ip')
+	.then(function(response) {
+		console.log(response);
+		return response.json();
+	})
+	.then(function(data) {
+		console.log(data);
+	})
+	.catch(function(err) {
+		console.log(err);
+	});
+
+fetch('http://httpbin.org/post', {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json' 
+	},
+	mode: 'cors',
+	body: JSON.stringify({message: 'Does this work?'})
+})
+	.then(function(response) {
+		console.log(response);
+		return response.json();
+	})
+	.then(function(data) {
+		console.log(data);
+	})
+	.catch(function(err) {
+		console.log(err);
+	});
 
 // promise.then(function(text) {
 // 	return text;
