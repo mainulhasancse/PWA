107844bd661cc9ed00eb629ca8a88abd67feccc5
Mainul Hasan <mainul@mvisolutions.com>
2020-02-10 11:18:11 +0600

    Add image at runtime

--- first-pwa/public/src/js/feed.js
+++ first-pwa/public/src/js/feed.js
@@ -1,6 +1,8 @@
 var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
+
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
@@ -28,32 +30,41 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
-// function createCard() {
-//   var cardWrapper = document.createElement('div');
-//   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
-//   var cardTitle = document.createElement('div');
-//   cardTitle.className = 'mdl-card__title';
-//   cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
-//   cardTitle.style.backgroundSize = 'cover';
-//   cardTitle.style.height = '180px';
-//   cardWrapper.appendChild(cardTitle);
-//   var cardTitleTextElement = document.createElement('h2');
-//   cardTitleTextElement.className = 'mdl-card__title-text';
-//   cardTitleTextElement.textContent = 'San Francisco Trip';
-//   cardTitle.appendChild(cardTitleTextElement);
-//   var cardSupportingText = document.createElement('div');
-//   cardSupportingText.className = 'mdl-card__supporting-text';
-//   cardSupportingText.textContent = 'In San Francisco';
-//   cardSupportingText.style.textAlign = 'center';
-//   cardWrapper.appendChild(cardSupportingText);
-//   componentHandler.upgradeElement(cardWrapper);
-//   sharedMomentsArea.appendChild(cardWrapper);
-// }
+function onSaveButtonClicked(event) {
+  console.log('Clicked');
+}
 
-// fetch('https://httpbin.org/get')
-//   .then(function(res) {
-//     return res.json();
-//   })
-//   .then(function(data) {
-//     createCard();
-//   });
+function createCard() {
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.style.color = 'black';
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = 'San Francisco Trip';
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__su pporting-text';
+  cardSupportingText.textContent = 'In San Francisco';
+  cardSupportingText.style.textAlign = 'center';
+  var cardSaveButton = document.createElement('button');
+  cardSaveButton.textContent = 'Save';
+  cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  cardSupportingText.appendChild(cardSaveButton);
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch('https://httpbin.org/get')
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    createCard();
+  });
--- first-pwa/public/sw.js
+++ first-pwa/public/sw.js
@@ -1,7 +1,10 @@
+var CACHE_STATIC_NAME = 'static-v6';
+var CACHE_DYNAMIC_NAME = 'dynamic-2';
+
 self. addEventListener('install', function(event) {
 	console.log('[Service Worker] Installing Service Worker ....', event);
 	event.waitUntil(
-		caches.open('static')
+		caches.open(CACHE_STATIC_NAME)
 			.then(function(cache) {
 				console.log('[Service Worker] Precaching App Shell....');
 				cache.addAll([
@@ -26,6 +29,17 @@ self. addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
 	// console.log('[Service Worker] Activating Service Worker ....', event);
+	event.waitUntil(
+		caches.keys()
+			.then(function(keyList) {
+				return Promise.all(keyList.map(function(key){
+					if(key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+						console.log('[Service Worker] Removing old cache.', key);
+						return caches.delete(key);
+					}
+				}));
+			})
+	);
 	return self.clients.claim();
 });
 
@@ -40,7 +54,7 @@ self.addEventListener('fetch', function(event) {
 				} else {
 					return fetch(event.request)
 						.then(function(res) {
-							return caches.open('dynamic')
+							return caches.open(CACHE_DYNAMIC_NAME)  
 								.then(function(cache) {
 									cache.put(event.request.url, res.clone());
 									return res;
@@ -53,3 +67,6 @@ self.addEventListener('fetch', function(event) {
 			})
 		);
 });
+
+
+
