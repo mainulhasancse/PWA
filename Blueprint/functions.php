75b7a435062610855c4a71cc01b957209f8a5a92
Mainul Hasan <mainul@mvisolutions.com>
2021-06-15 12:59:37 +0600

    Make PWA blueprint for WP

---
+++ Blueprint/functions.php
@@ -0,0 +1,25 @@
+<script type="text/javascript">
+    var PWA_CACHE_VERSION = 10
+</script>
+<?php
+add_action('wp_footer', 'install_service_worker');
+function install_service_worker() { ?>
+    <script>
+        if (!window.Promise) {
+            window.Promise = Promise;
+        }
+
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function() {
+                navigator.serviceWorker.register('/service-worker.js?VER=' + PWA_CACHE_VERSION)
+                .then(reg => {
+                    console.log('Service worker registered!', reg);
+                })
+                .catch(err => {
+                    console.log('Service worker registration failed: ', err);
+                });
+            });
+        }
+    </script>
+
+<?php } ?>
--- Blueprint/service-worker-wp.js
+++ Blueprint/service-worker-wp.js
@@ -1,8 +1,10 @@
-var CACHE_STATIC_NAME = 'static-v1';
-var CACHE_DYNAMIC_NAME = 'dynamic-v1';
-var EXCLUDE_URL_CACHING = ['/amp', '/wp-includes', '/admin', '/admin/*', '/manifest.json', '/robots.txt' ,'/wp-login.php', '/service-worker.js', '/.well-known/*'];
+var CACHE_VERSION = new URL(location).searchParams.get('VER');
+var CACHE_STATIC_NAME = 'static-v' + CACHE_VERSION;
+var CACHE_DYNAMIC_NAME = 'dynamic-v' + CACHE_VERSION;
+var EXCLUDE_URL_CACHING = ['/amp', '/wp-includes', '/wp-admin', '/wp-admin/*', '/manifest.json', '/robots.txt', '/wp-login.php', '/service-worker.js', '/.well-known/*'];
+var arrayLength = EXCLUDE_URL_CACHING.length;
 var STATIC_FILES = [
-'/offline/'
+    '/offline/'
 ];
 
 self.addEventListener('install', function (event) {
@@ -10,66 +12,60 @@ self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
         caches.open(CACHE_STATIC_NAME)
-        .then(function (cache) {
-            console.log('[Service Worker] Precaching App Shell');
-            cache.addAll(STATIC_FILES);
-        })
-        )
+            .then(function (cache) {
+                console.log('[Service Worker] Precaching App Shell');
+                cache.addAll(STATIC_FILES);
+            })
+    )
 });
 
 self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating Service Worker ....', event);
     event.waitUntil(
         caches.keys()
-        .then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-                    console.log('[Service Worker] Removing old cache.', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
-        );
+            .then(function (keyList) {
+                return Promise.all(keyList.map(function (key) {
+                    if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                        console.log('[Service Worker] Removing old cache.', key);
+                        return caches.delete(key);
+                    }
+                }));
+            })
+    );
     return self.clients.claim();
 });
 
-function avoid_caching (response=null, event=null) {
-
-    if(EXCLUDE_URL_CACHING.includes(event.url)){
-        return true;
-    }
-
-    // Check if we received a valid response
-    if(!response || response.status !== 200 || response.type !== 'basic') {
-        return true;
-    }
-
-    if(event.method === 'POST' || event.method === 'PUT' || event.method === 'PATCH' || event.method === 'DELETE'){
-        return true;
-    }
-
-    return false;
-
-}
-
 // cache then network strategy
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+        caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
             return cache.match(event.request).then(function (response) {
-                return response || fetch(event.request).then(function(response) {
-                    if(avoid_caching(response, event.request) === false){
-                        cache.put(event.request, response.clone());
+                return response || fetch(event.request).then(function (response) {
+                    // Doesn't cache following url
+                    for (var i = 0; i < arrayLength; i++) {
+                        if (event.request.url.indexOf(EXCLUDE_URL_CACHING[i]) !== -1) {
+                            return response;
+                        }
                     }
+
+                    // Check if we received a valid response
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    if (event.request.method === 'POST' || event.request.method === 'PUT' || event.request.method === 'PATCH' || event.request.method === 'DELETE') {
+                        return response;
+                    }
+                    cache.put(event.request, response.clone());
                     return response;
                 });
             });
-        }).catch(function(err) {
-
+        }).catch(function (err) {
+            // If both fail, show a generic fallback:
             return caches.open(CACHE_STATIC_NAME)
-            .then(function (cache) {
-                return cache.match('/offline/');
-            })
+                .then(function (cache) {
+                    return cache.match('/offline/');
+                })
         })
     );
 });
